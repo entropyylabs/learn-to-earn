0dad56ffb1f8c0f98eeb7f965cf20335894987ee
Ayush Jain <ayushjain2205@gmail.com>
2022-02-27 23:39:25 +0530

    Modify styling

--- src/Pages/CreateCourse.js
+++ src/Pages/CreateCourse.js
@@ -3,13 +3,22 @@ import { Collapse } from "antd";
 import AddCourse from "../Components/AddCourse";
 import AddModules from "../Components/AddModules";
 import AddReward from "../Components/AddReward";
+import createCourseBanner from "../assets/createCourseBanner.png";
+import Icon, { RightCircleTwoTone } from "@ant-design/icons";
 const { Panel } = Collapse;
 
 function CreateCourse() {
   return (
     <div className="page">
       <h1 className="page-title">Create Course</h1>
-      <Collapse accordion>
+      <Collapse
+        accordion
+        bordered={false}
+        expandIconPosition="right"
+        expandIcon={({ isActive }) => (
+          <RightCircleTwoTone rotate={isActive ? 90 : 0} />
+        )}
+      >
         <Panel header="Course Details" key="1">
           <AddCourse />
         </Panel>
@@ -20,6 +29,12 @@ function CreateCourse() {
           <AddReward />
         </Panel>
       </Collapse>
+      <img
+        src={createCourseBanner}
+        alt=""
+        srcset=""
+        style={{ marginTop: "50px" }}
+      />
     </div>
   );
 }
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,27 +2,31 @@ import React from "react";
 import { Button } from "antd";
 import CourseCard from "../Components/CourseCard";
 import { Row, Col, Slider } from "antd";
+import { Link } from "react-router-dom";
+import course1 from "../assets/course1.png";
+import course2 from "../assets/course2.png";
 
 function Home() {
   return (
     <div className="page">
-      <h6 className="page-title">Explore Courses</h6>
+      <Row>
+        <Col span={24}>
+          <div className="home-heading">
+            <h6 className="page-title">Explore Courses</h6>
+            <Button className="course-btn l2e-btn" type="primary">
+              <Link to="/create">Create course</Link>
+            </Button>
+          </div>
+        </Col>
+      </Row>
 
       <Row>
         <Col span={12}>
-          <CourseCard />
+          <CourseCard icon={course1} color="#D989FF" />
         </Col>
 
         <Col span={12}>
-          <CourseCard />
-        </Col>
-
-        <Col span={12}>
-          <CourseCard />
-        </Col>
-
-        <Col span={12}>
-          <CourseCard />
+          <CourseCard icon={course2} color="#18B2F4" />
         </Col>
       </Row>
     </div>
